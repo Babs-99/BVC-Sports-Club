68c06955a053e1554b510865cde2e968729097dd
Babs <ababalola16@alustudent.com>
2024-04-06 13:01:17 -0600

    updated the frontend

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#radioForm').click(function(event) {
+    $('#radioButton').click(function(event) {
         event.preventDefault(); // Prevent default form submission
 
         // Get the selected radio button value
@@ -8,37 +8,26 @@ $(document).ready(function() {
         var fullName = $('input[name="fullName"]').val();
         var address =$('input[name="address"]').val();
 
+        console.log(status, userID, fullName, address);
 
-        // $.ajax({
-        //     url: '/api/data',
-        //     method: 'GET',
-        //     dataType: 'json',
-        //     success: function(response) {
-        //       var userData = $('#userData');
-        //       userData.append('<p>Name: ' + response.name + '</p>');
-        //       userData.append('<p>Age: ' + response.age + '</p>');
-        //       userData.append('<p>City: ' + response.city + '</p>');
-        //     },
-        //     error: function(xhr, status, error) {
-        //       console.log('Error:', error);
-        //     }
-        //   });
 
-        // Send the selected value to the server
         $.ajax({
             type: 'POST',
             dataType: 'json',
-            contentType: 'application/json',
             url: 'http://localhost:3000/register',
             // url: 'https://registrationwebsitebackend-2.onrender.com',
             
             data: { 
-                studentNumber: userID,
+                userID: userID,
                 fullName: fullName,
                 address: address,
                 status: status },
             success: function(response) {
                 console.log('Data saved successfully:', response);
+                window.location.href = '/page/index.html?userID=' + encodeURIComponent(response.userID) +
+                '&fullName=' + encodeURIComponent(response.fullName) +
+                '&address=' + encodeURIComponent(response.address) +
+                '&status=' + encodeURIComponent(response.status);
             },
             error: function(error) {
                 console.log('Error saving data:', error);
