fadff83d50a46bce77f7a64748f466aeeb93853b
Babs <ababalola16@alustudent.com>
2024-04-06 17:34:57 -0600

    added some styling

--- page/script.js
+++ page/script.js
@@ -1,29 +1,27 @@
 const info = document.getElementById('info');
-// Parse URL parameters
+
 const urlParams = new URLSearchParams(window.location.search);
 const userID = urlParams.get('userID');
 const fullName = urlParams.get('fullName');
 const address = urlParams.get('address');
 const status = urlParams.get('status');
 
-// Display registration info on the page
-
 info.innerHTML = `
-    <p><strong>Student Number:</strong> ${userID}</p>
-    <p><strong>Full Name:</strong> ${fullName}</p>
-    <p><strong>Address:</strong> ${address}</p>
-    <p><strong>Status:</strong> ${status}</p>
+    <p><b>Student Number:</b> ${userID}</p>
+    <p><b>Full Name:</b> ${fullName}</p>
+    <p><b>Address:</b> ${address}</p>
+    <p><b>Status:</b> ${status}</p>
 `;
 
 if(status == "Volunteer"){
     info.innerHTML += 
-    `<p><strong>Fee:</strong> $0</p>`
+    `<p><b>Fee:</b> $0</p>`
 }
 if(status == "Staff"){
     info.innerHTML += 
-    `<p><strong>Fee:</strong> $50</p>`
+    `<p><b>Fee:</b> $50</p>`
 }
 if(status == "Student"){
     info.innerHTML += 
-    `<p><strong>Fee:</strong> $10</p>`
+    `<p><b>Fee:</b> $10</p>`
 }
